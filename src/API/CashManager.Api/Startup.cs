a9fa4425ba477538728b4f2c90c7558ef58c51d5
Felipe Freitas <fsfreitas@outlook.com>
2015-11-21 18:43:46 -0200

    Added configurations for API and Data layer.

--- src/API/CashManager.Api/Startup.cs
+++ src/API/CashManager.Api/Startup.cs
@@ -1,18 +1,15 @@
-﻿using System;
+﻿using Owin;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Builder;
-using Microsoft.AspNet.Hosting;
-using Microsoft.AspNet.Http;
-using Microsoft.AspNet.Routing;
+using System.Configuration;
+using System.Diagnostics;
 using Microsoft.Framework.DependencyInjection;
 
-namespace CashManager
+namespace CashManager.API
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env)
+        public Startup(IAppBuilder app)
         {
         }
 
@@ -20,22 +17,10 @@ namespace CashManager
         // Use this method to add services to the container
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            
             // Uncomment the following line to add Web API services which makes it easier to port Web API 2 controllers.
             // You will also need to add the Microsoft.AspNet.Mvc.WebApiCompatShim package to the 'dependencies' section of project.json.
             // services.AddWebApiConventions();
-        }
-
-        // Configure is called after ConfigureServices is called.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            // Configure the HTTP request pipeline.
-            app.UseStaticFiles();
-
-            // Add MVC to the request pipeline.
-            app.UseMvc();
-            // Add the following route for porting Web API 2 controllers.
-            // routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
         }
     }
 }
