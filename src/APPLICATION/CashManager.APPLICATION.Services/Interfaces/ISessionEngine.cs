051640f72caf2a31d75246b7687cd96fa1c0f4ac
Felipe Freitas <fsfreitas@outlook.com>
2015-12-10 23:25:10 -0200

    Added SessionEngine contract

---
+++ src/APPLICATION/CashManager.APPLICATION.Services/Interfaces/ISessionEngine.cs
@@ -0,0 +1,16 @@
+﻿using CashManager.DOMAIN.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashManager.APPLICATION.Services.Interfaces
+{
+    public interface ISessionEngine
+    {
+        Session StartSession();
+        Session FinishSession(Session session);
+        Session RemoveSession(Session session);
+    }
+}
