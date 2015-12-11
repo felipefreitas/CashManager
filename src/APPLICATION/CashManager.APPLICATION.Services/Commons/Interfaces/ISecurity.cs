8f897aedf30ee753577b317c5f66b5db0509155a
Felipe Freitas <fsfreitas@outlook.com>
2015-12-10 23:26:06 -0200

    Added contracts for engines.

---
+++ src/APPLICATION/CashManager.APPLICATION.Services/Commons/Interfaces/ISecurity.cs
@@ -0,0 +1,14 @@
+﻿using CashManager.DOMAIN.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashManager.APPLICATION.Services.Commons.Interfaces
+{
+    public interface ISecurity
+    {
+        User CurrentUser { get; set; }
+    }
+}
